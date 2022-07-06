f4475655f25e1a921eaa1da98856f7fd03f86519
Your Name <you@example.com>
2022-07-06 16:40:26 -0300

    Resolve

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,8 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert();
+	let edad;
+	edad = prompt('Ingrese Su Nombre');
+	document.getElementById("txtIdNombre").value = edad;
 }
 
