214afcd9661d7b17c09aebaca8368a43873ed972
Tomas Barbuto <barbutotomas@gmail.com>
2022-07-07 17:56:20 -0300

    Resuelto

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,17 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	//Declaro variables
+	let sueldo;
+	let resultado;
 
-	alert("ok");
+	//asigno a la variable el valor ingresado en caja con .value
+	sueldo = document.getElementById("txtIdSueldo").value;
+
+	//Relizamos el calculo
+	resultado = parseInt(sueldo)*1.10;
+
+	//Imprimo por ID el resultado.
+	document.getElementById("txtIdResultado").value = resultado;
+
 }
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,16 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	//Declaro variables
+	let importe;
+	let resultado;
+
+	//asigno a la variable el valor ingresado en caja con .value
+	importe = document.getElementById("txtIdImporte").value;
+
+	//Relizamos el calculo
+	resultado = parseInt(importe)*0.75;
+
+	//Imprimo por ID el resultado.
+	document.getElementById("txtIdResultado").value = resultado;
 }
