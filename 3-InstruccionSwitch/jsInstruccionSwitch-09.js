a60861f3a48515ddc6a247e2b7c6a0c91c2b9a28
Your Name <you@example.com>
2022-07-14 19:03:40 -0300

    Resuelto

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -3,8 +3,6 @@ function mostrar(){
 	let estacionDelAnio;
 	let destino;
 	let precioFinal;
-	let aumento;
-	let descuento;
 	const PRECIO = 15000;
 
 	estacionDelAnio = document.getElementById("txtIdEstacion").value;
@@ -24,16 +22,46 @@ function mostrar(){
 					precioFinal = PRECIO - (PRECIO * 0.10);
 					break;
 			}
+			break;
+		case "Verano":
+			switch(destino){
+				case "Bariloche":
+					precioFinal = PRECIO - (PRECIO * 0.20);
+					break;
+				case "Mar del plata":
+					precioFinal = PRECIO + (PRECIO * 0.20);
+					break;
+				case "Cataratas":
+				case "Cordoba":
+					precioFinal = PRECIO - (PRECIO * 0.10);
+					break;
+			}
+			break;
+		case "Otoño":
+		case "Primavera":
+			switch(destino){
+				case "Bariloche":
+				case "Mar del plata":
+				case "Cataratas":
+					precioFinal = PRECIO + (PRECIO * 0.10);
+					break;
+				case "Cordoba":
+					precioFinal = PRECIO;
+					break;
+			}
+			break;
 	}
+	alert(`El Precio final es: ${precioFinal}`);
 
 
 }//FIN DE LA FUNCIÓN
 /* Enunciado:
+en Otoño y Primavera: bariloche tiene un aumento del 10% cataratas tiene un aumento del 10% Mar del plata tiene un aumento del 10% y Cordoba tiene el precio sin descuento
+
 una agencia de viajes debe sacar las tarifas de los viajes , se cobra $15.000 por cada estadia como base, se pide el ingreso de una estacion del año y localidad para vacacionar para poder calcular el precio final
 
 en Invierno: bariloche tiene un aumento del 20% cataratas y Cordoba tiene un descuento del 10% Mar del plata tiene un descuento del 20%
 
 en Verano: bariloche tiene un descuento del 20% cataratas y Cordoba tiene un aumento del 10% Mar del plata tiene un aumento del 20%
 
-en Otoño y Primavera: bariloche tiene un aumento del 10% cataratas tiene un aumento del 10% Mar del plata tiene un aumento del 10% y Cordoba tiene el precio sin descuento
  */
--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,48 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let estacionDelAnio;
+	let destino;
+
+	estacionDelAnio = document.getElementById("txtIdEstacion").value;
+	destino = document.getElementById("txtIdDestino").value;
+
+	switch(estacionDelAnio){
+		case "Invierno":
+				if(destino == "Bariloche"){
+					alert("Se viaja");
+				}else{
+					alert("No se viaja");
+				}
+			break;
+		case "Verano":
+				if(destino == "Mar del plata" || destino == "Cataratas"){
+					alert("Se viaja");
+				}else{
+					alert("No se viaja");
+				}
+			break;
+		case "Otoño":
+				alert("Se viaja a todos los destinos");
+			break;
+		case "Primavera":
+				if(destino != "Bariloche"){
+					alert("Se viaja");
+				}else{
+					alert("No se viaja");
+				}
+			break;
+	}
 
 }//FIN DE LA FUNCIÓN
+
+/*
+ una agencia de viajes nos piden informar si hacemos viajes a lugares según la estación del año estemos, informar si "Se viaja" o "No se viaja" a ese lugar
+
+en Invierno: Solo Bariloche informa "se viaja" los demas destinos "No se viaja"
+
+en Verano: Se viaja a Mar del plata y Cataratas solamente
+
+en Otoño: Se viaja a todos los destinos.
+
+primavera: solo no se viaja a Bariloche
+ */
